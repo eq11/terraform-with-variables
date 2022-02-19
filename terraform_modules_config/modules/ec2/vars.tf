83e30d2d16d18b93bf8c8b6cd7407acd0ae70f96
eq11 <98138963+eq11@users.noreply.github.com>
2022-02-19 02:35:29 -0500

    Create vars.tf

---
+++ terraform_modules_config/modules/ec2/vars.tf
@@ -0,0 +1,29 @@
+variable "image_id" {
+  type        = string
+  description = "ami"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "subnet_id"
+}
+
+variable "vpc_sg" {
+  type        = string
+  description = "vpc_secuirty_group"
+}
+
+variable "key_file" {
+  type        = string
+  description = "pem_key"
+}
+
+variable "instance" {
+  type        = string
+  description = "instance_type"
+}
+
+variable "monitor" {
+  type        = string
+  description = "monitoring enabled true or false"
+}
