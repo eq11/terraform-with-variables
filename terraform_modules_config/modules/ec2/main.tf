83a7688cf4424552b0bb6b1e81d4c733b6ffee28
eq11 <98138963+eq11@users.noreply.github.com>
2022-02-19 02:34:51 -0500

    Create main.tf

---
+++ terraform_modules_config/modules/ec2/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "terraform-ec2" {
+  ami           = var.image_id
+  instance_type = var.instance
+  key_name      = var.key_file
+  monitoring    = var.monitor
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [
+    var.vpc_sg
+  ]
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
